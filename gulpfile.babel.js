6baabf6995831d47ef7c286ee6510cecfd7cacfe
Ігор <49112525+igorulich@users.noreply.github.com>
2023-02-11 16:38:18 +0200

    Update gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,21 +3,21 @@ import requireDir from "require-dir";
 
 import { series, parallel } from "gulp";
 
-import { Webp } from "./tasks/Webp";
-import { resources } from "./tasks/resources";
-import { clean } from "./tasks/clean";
-import { icons } from "./tasks/icons";
-import { svgIcons } from "./tasks/svgIcons";
-import { server } from "./tasks/server";
-import { js } from "./tasks/js";
-import { jsBuild } from "./tasks/jsbuid";
-import { html } from "./tasks/html";
-import { css } from "./tasks/css";
-import { cssBuild } from "./tasks/cssbuild";
-import { watchFiles } from "./tasks/watchFiles";
-import { moveFontsWoff2 } from "./tasks/moveFontsWooff2";
-import { moveFontsWoff } from "./tasks/moveFontsWooff";
-import { connectionFonts } from "./tasks/connectionFonts"
+import { Webp } from "./tasks/Webp";/*форматуемо фото в webp*/
+import { resources } from "./tasks/resources";/*переносимо допоміжні матеріали в корінь проекту такі як audio video favicon*/
+import { clean } from "./tasks/clean";/*видаленя папок з файлами з старим кодом*/
+import { icons } from "./tasks/icons";/*створення спрайтів*/
+import { svgIcons } from "./tasks/svgIcons";/*перенесеня спрайтів зи стилями в корневу папку src*/
+import { server } from "./tasks/server";/*запуск програми на локальному сервері*/
+import { js } from "./tasks/js";/*компиляція js файлів*/
+import { jsBuild } from "./tasks/jsbuid";/*компиляция js файлів перед продакшеном*/
+import { html } from "./tasks/html";/*компіляція html файлів*/
+import { css } from "./tasks/css";/*компіляція css файлів*/
+import { cssBuild } from "./tasks/cssbuild";/*компіляція css файлів перед продакшеном*/
+import { watchFiles } from "./tasks/watchFiles";/*відсліжування змін у коді файлів*/
+import { moveFontsWoff2 } from "./tasks/moveFontsWooff2";/*створення ttf2woff файла з шрифтами*/
+import { moveFontsWoff } from "./tasks/moveFontsWooff";/*створення ttf2woff2 файла з шрифтами*/
+import { connectionFonts } from "./tasks/connectionFonts"/*створення підключення шрифтів різноі конфігурації та стилю*/
 
 //Dev
 
