a1eb8d4349a505ab9d6d236e73aea89ce142521b
Ігор <49112525+igorulich@users.noreply.github.com>
2023-02-12 09:42:46 +0200

    Update js.js

--- tasks/js.js
+++ tasks/js.js
@@ -1,17 +1,22 @@
 "use strict";
-import { config } from "../configModul ";
-import { src, dest } from "gulp";
-import plumber from "gulp-plumber";
-import sourcemaps from "gulp-sourcemaps";
-import rename from "gulp-rename";
-import browserSync from "browser-sync";
-import webpackStream from "webpack-stream";
-import livereload from "gulp-livereload";
-export function js(cb) {
-	return src(config.src.js)
-		.pipe(plumber())
+import { config } from "../configModul ";/*підключення файла з конфігураціями*/
+import { src, dest } from "gulp";/*підключення gulp зйого можливими  виконання gulpкоманди прописаними у фігурних дужках{обовязково прописуемо}*/
+import plumber from "gulp-plumber";/*Коротко кажучи, він замінює pipeметод і видаляє стандартний onerrorобробник error події,
+який за замовчуванням відключає потоки в разі помилки. */
+import sourcemaps from "gulp-sourcemaps";/*Вбудовані вихідні карти вбудовані у вихідний файл.*/
+import rename from "gulp-rename";/*gulp-rename надає прості методи перейменування файлів.*/
+import browserSync from "browser-sync";/*Налаштуйте набір налаштувань синхронізації з інтерфейсу користувача або командного рядка, 
+щоб створити персоналізоване тестове середовище*/
+import webpackStream from "webpack-stream";/*буде скомпільовано .js в активи за допомогою webpack у dist/з іменем вихідного файлу [hash].js
+(генерований webpack хеш збірки).*/
+import livereload from "gulp-livereload";/*gulp-livereloadне буде автоматично відстежувати зміни.
+Тепер вам доведеться викликати вручну livereload.listen, якщо ви не встановите опцію start:*/
+export function js(cb) {/*експортуемо tasks js*/
+	return src(config.src.js)/*беремо всі вихідні файли які прописання у нас у файлі конфігурації*/
+		.pipe(plumber())/*пропускаемо через плагін для уникненя помилок*/
 		.pipe(
-			webpackStream({
+			webpackStream({;/*берем свій файл обовязково мае назву main.js* пропускаемо через webpackStream та за допомогою babel-loader пересобираемо в один та міняю назву на
+			scripts.js*/
 				mode: "development",
 				output: {
 					filename: "main.js"
@@ -43,10 +48,10 @@ export function js(cb) {
 				extname: ".js",
 			})
 		)
-		.pipe(plumber.stop())
-		.pipe(sourcemaps.write("."))
-		.pipe(dest(config.build.js))
-		.pipe(browserSync.reload({ stream: true }))
-		.pipe(livereload()),
+		.pipe(plumber.stop())/*зачиняется плагін для запобіганью помилок*/
+		.pipe(sourcemaps.write("."))/*зачиняемо плагин для створеня карти і указуемо крапкою для зберіганя у цьому з ісходним файлом положеню*/
+		.pipe(dest(config.build.js))/*та виводимо у кінцеву папку*/
+		.pipe(browserSync.reload({ stream: true }))/*відночасно запускаемо браузер для відтвореня у режимі онлайн у нашому проекті*/
+		.pipe(livereload()),/*буде автоматично відстежувати зміни*/
 		cb();
 };
