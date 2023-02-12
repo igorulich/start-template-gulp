23b85d5929fa4774dadacad1216ae9cb350fad5c
Ігор <49112525+igorulich@users.noreply.github.com>
2023-02-12 07:00:26 +0200

    Update Webp.js

--- tasks/Webp.js
+++ tasks/Webp.js
@@ -1,12 +1,13 @@
 "use strict";
-import { config } from "../configModul ";
-import { src, dest } from "gulp";
-const cwebp = require("gulp-cwebp");
-import browserSync from "browser-sync";
-export const Webp = (cb) => {
-	return src(config.src.images)
-.pipe(cwebp())
-		.pipe(dest(config.build.images))
-		.pipe(browserSync.reload({ stream: true })),
+import { config } from "../configModul ";/*підключення файла з конфігураціями*/
+import { src, dest } from "gulp";/*підключення gulp зйого можливими  виконання gulpкоманди прописаними у фігурних дужках{обовязково прописуемо}*/
+const cwebp = require("gulp-cwebp");/*підключення бібліотеки для створення зображень у данному форматі Webp.*/
+import browserSync from "browser-sync";/*Налаштуйте набір налаштувань синхронізації з інтерфейсу користувача або командного рядка, 
+щоб створити персоналізоване тестове середовище*/
+export const Webp = (cb) => {/*експортуемо tasks Webp*/
+	return src(config.src.images)/*беремо всі вихідні файли які прописання у нас у файлі конфігурації*/
+.pipe(cwebp())/*пропускаемо щерез плагин cwebp з допомогою функції .pipe вбудованої у сутність плагіну*/
+		.pipe(dest(config.build.images))/*та виводимо у кінцеву папку*/
+		.pipe(browserSync.reload({ stream: true })),/*відночасно запускаемо браузер для відтвореня у режимі онлайн у нашому проекті*/
 	cb();
 };
