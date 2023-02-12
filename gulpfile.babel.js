221c0763543d01d354b38abbd304921fa4db5a94
Ігор <49112525+igorulich@users.noreply.github.com>
2023-02-12 05:39:38 +0200

    Update gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,21 +3,21 @@ import requireDir from "require-dir";
 /*импортуемо конфігурацфї бібліотек*/
 import { series, parallel } from "gulp";
 
-import { Webp } from "./tasks/Webp";/*імпорт функції для форматуемо фото в webp*/
-import { resources } from "./tasks/resources";/*імпорт функції для переносимо допоміжні матеріали в корінь проекту такі як audio video favicon*/
-import { clean } from "./tasks/clean";/*імпорт функції для видаленя папок з файлами з старим кодом*/
-import { icons } from "./tasks/icons";/*створення спрайтів*/
-import { svgIcons } from "./tasks/svgIcons";/*перенесеня спрайтів зи стилями в корневу папку src*/
-import { server } from "./tasks/server";/*запуск програми на локальному сервері*/
-import { js } from "./tasks/js";/*компиляція js файлів*/
-import { jsBuild } from "./tasks/jsbuid";/*компиляция js файлів перед продакшеном*/
-import { html } from "./tasks/html";/*компіляція html файлів*/
-import { css } from "./tasks/css";/*компіляція css файлів*/
-import { cssBuild } from "./tasks/cssbuild";/*компіляція css файлів перед продакшеном*/
-import { watchFiles } from "./tasks/watchFiles";/*відсліжування змін у коді файлів*/
-import { moveFontsWoff2 } from "./tasks/moveFontsWooff2";/*створення ttf2woff файла з шрифтами*/
-import { moveFontsWoff } from "./tasks/moveFontsWooff";/*створення ttf2woff2 файла з шрифтами*/
-import { connectionFonts } from "./tasks/connectionFonts"/*створення підключення шрифтів різноі конфігурації та стилю*/
+import { Webp } from "./tasks/Webp";/*імпорт tasks для форматуемо фото в webp*/
+import { resources } from "./tasks/resources";/*імпорт tasks для переносимо допоміжні матеріали в корінь проекту такі як audio video favicon*/
+import { clean } from "./tasks/clean";/*імпорт tasks для видаленя папок з файлами з старим кодом*/
+import { icons } from "./tasks/icons";/*імпорт tasks для створення спрайтів*/
+import { svgIcons } from "./tasks/svgIcons";/*імпорт tasks для перенесеня спрайтів зи стилями в корневу папку src*/
+import { server } from "./tasks/server";/*імпорт tasks для запуск програми на локальному сервері*/
+import { js } from "./tasks/js";/*імпорт tasks для компиляція js файлів*/
+import { jsBuild } from "./tasks/jsbuid";/*імпорт tasks для компиляция js файлів перед продакшеном*/
+import { html } from "./tasks/html";/*імпорт tasks для компіляція html файлів*/
+import { css } from "./tasks/css";/*імпорт tasks для компіляція css файлів*/
+import { cssBuild } from "./tasks/cssbuild";/*імпорт tasks для компіляція css файлів перед продакшеном*/
+import { watchFiles } from "./tasks/watchFiles";/*імпорт tasks для відсліжування змін у коді файлів*/
+import { moveFontsWoff2 } from "./tasks/moveFontsWooff2";/*імпорт tasks для створення ttf2woff файла з шрифтами*/
+import { moveFontsWoff } from "./tasks/moveFontsWooff";/*імпорт tasks для створення ttf2woff2 файла з шрифтами*/
+import { connectionFonts } from "./tasks/connectionFonts"/*імпорт tasks для створення підключення шрифтів різноі конфігурації та стилю*/
 
 //Dev
 /*ЕКСПОРТУЕМО ФУНКЦІЇ*/
