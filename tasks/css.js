c42de613fd353ab252c4971fbfaed859498c7d03
Ігор <49112525+igorulich@users.noreply.github.com>
2023-02-12 07:30:00 +0200

    Update css.js

--- tasks/css.js
+++ tasks/css.js
@@ -1,15 +1,23 @@
 "use strict";
-import { config } from "../configModul ";
-import { src, dest } from "gulp";
-import plumber from "gulp-plumber";
-import sourcemaps from "gulp-sourcemaps";
-import autoprefixer from "gulp-autoprefixer";
-import cleanCSS from "gulp-clean-css";
-import rename from "gulp-rename";
-import browserSync from "browser-sync";
-import livereload from "gulp-livereload";
-import dartSass from "sass";
-import gulpSass from "gulp-sass";
+import { config } from "../configModul ";/*підключення файла з конфігураціями*/
+import { src, dest } from "gulp";/*підключення gulp зйого можливими  виконання gulpкоманди прописаними у фігурних дужках{обовязково прописуемо}*/
+import plumber from "gulp-plumber";/*Коротко кажучи, він замінює pipeметод і видаляє стандартний onerrorобробник errorподії,
+який за замовчуванням відключає потоки в разі помилки. */
+import sourcemaps from "gulp-sourcemaps";/*Вбудовані вихідні карти вбудовані у вихідний файл.*/
+import autoprefixer from "gulp-autoprefixer";/*Префікс CSS за допомогою автопрефікса*/
+import cleanCSS from "gulp-clean-css";/*Це лише простий плагін gulp ,
+що означає, що це не що інше, як тонка обгортка навколо clean-css. Якщо схоже, що у вас проблеми, пов’язані з CSS, зверніться до clean-css .*/
+import rename from "gulp-rename";/*gulp-rename надає прості методи перейменування файлів.*/
+import browserSync from "browser-sync";/*Налаштуйте набір налаштувань синхронізації з інтерфейсу користувача або командного рядка, 
+щоб створити персоналізоване тестове середовище*/
+import livereload from "gulp-livereload";/*gulp-livereloadне буде автоматично відстежувати зміни.
+Тепер вам доведеться викликати вручну livereload.listen, якщо ви не встановите опцію start:*/
+import dartSass from "sass";/*Щоб використовувати gulp-sassв модулі ECMAScript (який підтримується в новіших Node.js 14 і пізніших версіях),
+виконайте щось на зразок цього: */
+/*імпортувати  dartSass  із  'sass' ; 
+імпорт  gulpSass  з  'gulp-sass' ; 
+const  sass  =  gulpSass ( dartSass ) */
+import gulpSass from "gulp-sass";/*імпортуемо бібліотеку*/
 export const sass = gulpSass(dartSass);
 export function css(cb) {
 	return src(config.src.css)
