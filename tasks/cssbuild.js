fc48d42b0d5e44f778b68496e60cbcde028c21b9
Ігор <49112525+igorulich@users.noreply.github.com>
2023-02-12 08:24:48 +0200

    Update cssbuild.js

--- tasks/cssbuild.js
+++ tasks/cssbuild.js
@@ -10,21 +10,21 @@ import livereload from "gulp-livereload";
 import dartSass from "sass";
 import gulpSass from "gulp-sass";
 export const sass = gulpSass(dartSass);
-export function cssBuild(cb) {
-	return src(config.src.css)
-			.pipe(plumber())
-			.pipe(sass().on("error", sass.logError))
-			.pipe(autoprefixer(config.autoprefixerConfig))
-			.pipe(cleanCSS({ outputStyle: "compressed" }))
+export function cssBuild(cb) {/*експортуемо taskscssBuild*/
+	return src(config.src.css)/*беремо всі вихідні файли які прописання у нас у файлі конфігурації*/
+			.pipe(plumber())/*пропускаемо через плагін для уникненя помилок*/
+			.pipe(sass().on("error", sass.logError))/*пропускаемо через плагін sass для синхронного відтворення вашого CSS*/
+			.pipe(autoprefixer(config.autoprefixerConfig))/*пропускаемо через автопрефіксер для коректного відображення у більш старійших браузерах*/
+			.pipe(cleanCSS({ outputStyle: "compressed" }))/*тонка обгортка за допомогую вбудованій функції у плагін " compressed " зменьшуемо файл*/
 			.pipe(
-				rename({
+				rename({/*переіменовуем файл та змінюем розширення файлу з SCSS або SASS на CSS*/
 					basename: "styles",
 					extname: ".css",
 				})
 			)
-			.pipe(plumber.stop())
-			.pipe(dest(config.build.css))
-			.pipe(browserSync.reload({ stream: true }))
-			.pipe(livereload()),
+			.pipe(plumber.stop())/*зачиняется плагін для запобіганью помилок*/
+			.pipe(dest(config.build.css))/*та виводимо у кінцеву папку*/
+			.pipe(browserSync.reload({ stream: true }))/*відночасно запускаемо браузер для відтвореня у режимі онлайн у нашому проекті*/
+			.pipe(livereload()),/*буде автоматично відстежувати зміни*/
 		cb();
 }
