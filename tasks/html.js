06e3d7d2a50490a4dcff064e25b36cb79e50b921
Ігор <49112525+igorulich@users.noreply.github.com>
2023-02-12 08:57:29 +0200

    Update html.js

--- tasks/html.js
+++ tasks/html.js
@@ -1,21 +1,22 @@
 "use strict";
-import { config } from "../configModul ";
-import { src, dest } from "gulp";
-import fileinclude from "gulp-file-include";
+import { config } from "../configModul ";/*підключення файла з конфігураціями*/
+import { src, dest } from "gulp";/*підключення gulp зйого можливими  виконання gulpкоманди прописаними у фігурних дужках{обовязково прописуемо}*/
+import fileinclude from "gulp-file-include";/*плагін gulp для сбору та включення файлів html */
 import browserSync from "browser-sync";
 import livereload from "gulp-livereload";
-import gulpHtmlBemValidator from "gulp-html-bem-validator";
-export function html(cb) {
-	return src([config.src.html])
-		.pipe(
+import gulpHtmlBemValidator from "gulp-html-bem-validator";/*Плагін Gulp для перевірки bem html*/
+export function html(cb) {/*експортуемо tasks html*/
+	return src([config.src.html])/*беремо всі вихідні файли які прописання у нас у файлі конфігурації*/
+		.pipe(/*за допомогую плагіну собираемо в один файл*/
 			fileinclude({
 				prefix: "@",
 				basepath: "@file",
 			})
 		)
 	.pipe(gulpHtmlBemValidator())
-		.pipe(dest(config.build.html))
-		.pipe(browserSync.reload({ stream: true }))
-		.pipe(livereload()),
+		.pipe(dest(config.build.html))/*перевіряемо через плагін gulp-html-bem-validator на помилки він вас попередить у терміналі що ви зробили помилку та іїї покаже в 
+		terminal у вашому редакторі коду яки ви використовуете*/
+		.pipe(browserSync.reload({ stream: true }))/*відночасно запускаемо браузер для відтвореня у режимі онлайн у нашому проекті*/
+		.pipe(livereload()),/*буде автоматично відстежувати зміни*/
 		cb();
 }
