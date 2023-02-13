5e5c90a42db472a419e051a439b853938ac528fb
Ігор <49112525+igorulich@users.noreply.github.com>
2023-02-13 05:21:20 +0200

    Update server.js

--- tasks/server.js
+++ tasks/server.js
@@ -1,6 +1,7 @@
 "use strict";
-import { config } from "../configModul ";
-import browserSync from "browser-sync";
-export function server(cb) {
-	browserSync.init(config.serverConfig), cb();
+import { config } from "../configModul ";/*підключення файла з конфігураціями*/
+import browserSync from "browser-sync";;/*Налаштуйте набір налаштувань синхронізації з інтерфейсу користувача або командного рядка, 
+щоб створити персоналізоване тестове середовище*/
+export function server(cb) {/*експортуемо tasks server*/
+	browserSync.init(config.serverConfig), cb();/*запускаум локальний сервкр зналаштуванями які прописані у файлі конфігурації*/
 };
