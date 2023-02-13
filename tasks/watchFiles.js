b8c5b57bab03be1fcbee5f14f12051ddfaab9539
Ігор <49112525+igorulich@users.noreply.github.com>
2023-02-13 05:32:26 +0200

    Update watchFiles.js

--- tasks/watchFiles.js
+++ tasks/watchFiles.js
@@ -1,7 +1,8 @@
 "use strict";
-import { config } from "../configModul ";
-import { watch } from "gulp";
-import browserSync from "browser-sync";
+import { config } from "../configModul ";/*підключення файла з конфігураціями*/
+import { watch } from "gulp";/*підключення gulp зйого можливими  виконання gulpкоманди прописаними у фігурних дужках{обовязково прописуемо}*/
+import browserSync from "browser-sync";/*Налаштуйте набір налаштувань синхронізації з інтерфейсу користувача або командного рядка, 
+щоб створити персоналізоване тестове середовище*/
 import livereload from "gulp-livereload";
 import { Webp } from "./Webp";
 import { resources } from "./resources";
@@ -13,8 +14,8 @@ import { moveFontsWoff } from "./moveFontsWooff";
 import { moveFontsWoff2 } from "./moveFontsWooff2";
 import { connectionFonts } from "./connectionFonts";
 
-export function watchFiles(cb) {
-	livereload.listen();
+export function watchFiles(cb) {/*експортуемо tasks watchFiles*/
+	livereload.listen();/*буде автоматично відстежувати зміни*/
 	watch(config.watch.html, html).on("change", browserSync.reload);
 	watch(config.build.fonts, connectionFonts);
 	watch(config.watch.fonts, moveFontsWoff2);
