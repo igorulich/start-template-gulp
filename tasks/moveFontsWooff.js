a5d352b52917ec0c99dffceb18997b35f6d11400
Ігор <49112525+igorulich@users.noreply.github.com>
2023-02-13 05:10:39 +0200

    Update moveFontsWooff.js

--- tasks/moveFontsWooff.js
+++ tasks/moveFontsWooff.js
@@ -1,12 +1,12 @@
 "use strict";
-import { config } from "../configModul ";
-import { src, dest } from "gulp";
-import ttf2woff from "gulp-ttf2woff";
+import { config } from "../configModul ";/*підключення файла з конфігураціями*/
+import { src, dest } from "gulp";/*підключення gulp зйого можливими  виконання gulpкоманди прописаними у фігурних дужках{обовязково прописуемо}*/
+import ttf2woff from "gulp-ttf2woff";/*підключаемо плагин для конвертфції файлїв, які мають розширення .ttf. у формат ttf2woff*/
 
 
-export function moveFontsWoff (cb)  {
-	return src([config.src.fonts])
-		.pipe(ttf2woff())
-		.pipe(dest(config.build.fonts)),
+export function moveFontsWoff (cb)  {/*експортуемо tasks moveFontsWoff*/
+	return src([config.src.fonts])/*беремо всі вихідні файли у формат .ttf які прописання у вас у файлі конфігурації*/
+		.pipe(ttf2woff())/*пропускаемо через .pipe плагина*/
+		.pipe(dest(config.build.fonts)),/*та виводимо у кінцеву папку*/
 		cb();
 };
