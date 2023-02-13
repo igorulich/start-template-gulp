806b753325d25c9d1d709282917aeb81034e8d11
Ігор <49112525+igorulich@users.noreply.github.com>
2023-02-13 05:27:23 +0200

    Update svgIcons.js

--- tasks/svgIcons.js
+++ tasks/svgIcons.js
@@ -1,9 +1,10 @@
 "use strict";
-import { config } from "../configModul ";
-import { src, dest } from "gulp";
-import browserSync from "browser-sync";
-export function svgIcons(cb) {
-	return src(config.src.icons)
-		.pipe(dest(config.build.css))
-		.pipe(browserSync.reload({ stream: true })), cb();
+import { config } from "../configModul ";/*підключення файла з конфігураціями*/
+import { src, dest } from "gulp";/*підключення gulp зйого можливими  виконання gulpкоманди прописаними у фігурних дужках{обовязково прописуемо}*/
+import browserSync from "browser-sync";/*Налаштуйте набір налаштувань синхронізації з інтерфейсу користувача або командного рядка, 
+щоб створити персоналізоване тестове середовище*/
+export function svgIcons(cb) {/*експортуемо tasks svgIcons*/
+	return src(config.src.icons)/*беремо створений спрайт та переносим у кінцеву папку яка йде на продакшен*/
+		.pipe(dest(config.build.css))/*кінцува папка*/
+		.pipe(browserSync.reload({ stream: true })), cb();,/*відночасно запускаемо браузер для відтвореня у режимі онлайн у нашому проекті*/
 };
